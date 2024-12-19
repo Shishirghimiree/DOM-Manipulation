31c85be5f94824adc1f8c4a7ec0a3fc0a9748cee
shishirghimiree <shishirghimirecsit077@indrenicollege.edu.np>
2024-12-19 17:24:26 +0545

    DOM

---
+++ 02/script.js
@@ -0,0 +1,17 @@
+var cont = document.querySelector("#container")
+var love =document.querySelector("i")
+
+cont.addEventListener("dblclick",function(){
+    // love.style.color = "red"
+    love.style.transform=`scale(1)`;
+    love.style.opacity=0.8;
+
+    setTimeout(function(){
+        love.style.opacity =0;
+    },1000)
+
+    setTimeout(function(){
+        love.style.transform = `scale(0)`;
+    },2000)
+
+})
