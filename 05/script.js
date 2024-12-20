12fffe42942af570507aee0ff9c8469c183896d2
shishirghimiree <shishirghimirecsit077@indrenicollege.edu.np>
2024-12-20 14:06:49 +0545

    DOM

---
+++ 05/script.js
@@ -0,0 +1,24 @@
+var arr =[
+    {dp:"https://plus.unsplash.com/premium_photo-1669704098824-3bb06bb771f4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://images.unsplash.com/photo-1541519481457-763224276691?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1541519481457-763224276691?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://plus.unsplash.com/premium_photo-1669740217737-611345f9d246?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://plus.unsplash.com/premium_photo-1670282393309-70fd7f8eb1ef?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://plus.unsplash.com/premium_photo-1669704098776-1408db9ea894?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+]
+
+var stories =document.querySelector("#stories")
+var clutter="";
+arr.forEach(function(elem,idx){
+    clutter +=`<div id="story">
+          <img id="${idx}" src="${elem.dp}" alt=""/>
+        </div>`
+})
+stories.innerHTML = clutter
+
+stories.addEventListener("click",function(details){
+   document.querySelector("#full-screen").style.display ="block"
+   document.querySelector("#full-screen").style.backgroundImage =`url(${arr[details.target.id].story})`
+
+   setTimeout(function(){
+    document.querySelector("#full-screen").style.display ="none"
+   },3000)
+})
